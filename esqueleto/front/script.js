184a89b9332565198ac775a3b8aed9957c395b44
andreslires <andres.lires@udc.es>
2025-12-05 13:09:28 +0100

    Pequeños cambios

--- esqueleto/front/script.js
+++ esqueleto/front/script.js
@@ -8,7 +8,7 @@ var esriSatLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/serv
 // *********************Creación del mapa**************************
 
 var map = L.map('map'); 
-map.setView(new L.LatLng(43.25, -7.34), 16);
+map.setView(new L.LatLng(43.253, -7.34), 16);
 
 // Capa por defecto: 
 map.addLayer(esriSatLayer);
@@ -17,6 +17,9 @@ map.addLayer(esriSatLayer);
 // **************VACAS: Dentro y fuera de las fincas***************
 
 // Los datos se cargan conectandonos a GEOSERVER, que a su vez los obtiene de PostGIS
+
+// Los marcadores personalizados se han cambiado en GEOSERVER
+
 var vacasfuera = L.tileLayer.wms('http://localhost:8080/geoserver/wms', {
     layers: 'rxdet:vacas_fuera',
     format: 'image/png',
@@ -32,19 +35,12 @@ var vacasdentro = L.tileLayer.wms('http://localhost:8080/geoserver/wms', {
 // ****************************************************************
 // **************Control con botones*******************************
 
-// Coordenadas aproximadas del área de las vacas (ajusta según tus datos)
-const boundsVacas = [
-    [43.245, -7.345],  // Esquina suroeste
-    [43.255, -7.335]   // Esquina noreste
-];
-
 // Botón Vacas Dentro
 document.getElementById('btnDentro').addEventListener('click', function() {
     if (map.hasLayer(vacasdentro)) {
         map.removeLayer(vacasdentro);
     } else {
         map.addLayer(vacasdentro);
-        map.fitBounds(boundsVacas);
     }
 });
 
@@ -54,7 +50,6 @@ document.getElementById('btnFuera').addEventListener('click', function() {
         map.removeLayer(vacasfuera);
     } else {
         map.addLayer(vacasfuera);
-        map.fitBounds(boundsVacas);
     }
 });
 
