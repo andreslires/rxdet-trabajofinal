1f103c0d4c5a45e0593a30fcdef3bad269f74789
Ángel <angel.vilarino.garcia@udc.es>
2025-12-05 12:50:32 +0100

    Version buena 5

--- esqueleto/front/script.js
+++ esqueleto/front/script.js
@@ -1,25 +1,20 @@
 // ****************************************************************
 // *********************Capas base*********************************
 
-// OpenStreetMap
 // Mapa satelite:
 var esriSatLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}');
-
-// var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
 // ****************************************************************
 // *********************Creación del mapa**************************
 
-var map = L.map('map'); map.setView(new L.LatLng(43.25, -7.34), 15);
+var map = L.map('map'); 
+map.setView(new L.LatLng(43.25, -7.34), 16);
 
 // Capa por defecto: 
 map.addLayer(esriSatLayer);
 
-
 // ****************************************************************
 // **************VACAS: Dentro y fuera de las fincas***************
-
-// Marcadores de vacas dentro y fuera de las fincas
 
 // Los datos se cargan conectandonos a GEOSERVER, que a su vez los obtiene de PostGIS
 var vacasfuera = L.tileLayer.wms('http://localhost:8080/geoserver/wms', {
@@ -34,19 +29,47 @@ var vacasdentro = L.tileLayer.wms('http://localhost:8080/geoserver/wms', {
     transparent: true
 });
 
+// ****************************************************************
+// **************Control con botones*******************************
+
+// Coordenadas aproximadas del área de las vacas (ajusta según tus datos)
+const boundsVacas = [
+    [43.245, -7.345],  // Esquina suroeste
+    [43.255, -7.335]   // Esquina noreste
+];
+
+// Botón Vacas Dentro
+document.getElementById('btnDentro').addEventListener('click', function() {
+    if (map.hasLayer(vacasdentro)) {
+        map.removeLayer(vacasdentro);
+    } else {
+        map.addLayer(vacasdentro);
+        map.fitBounds(boundsVacas);
+    }
+});
+
+// Botón Vacas Fuera
+document.getElementById('btnFuera').addEventListener('click', function() {
+    if (map.hasLayer(vacasfuera)) {
+        map.removeLayer(vacasfuera);
+    } else {
+        map.addLayer(vacasfuera);
+        map.fitBounds(boundsVacas);
+    }
+});
+
+// Botón Limpiar Capas
+document.getElementById('btnLimpiar').addEventListener('click', function() {
+    if (map.hasLayer(vacasdentro)) {
+        map.removeLayer(vacasdentro);
+    }
+    if (map.hasLayer(vacasfuera)) {
+        map.removeLayer(vacasfuera);
+    }
+});
 
 // ****************************************************************
-// **************Overlays y controles******************************
-
-
-var overlayMaps = {
-    "Vacas fuera de las fincas": vacasfuera,
-    "Vacas dentro de las fincas": vacasdentro
-};
-
-// Control de capas
-var layerControl = L.control.layers(null, overlayMaps);
-map.addControl(layerControl);
+// **************Controles adicionales*****************************
 
 // Escala métrica
 var scaleControl = L.control.scale({ imperial: false });
